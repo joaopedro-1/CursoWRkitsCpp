c7f4dea6948bba9d15eef8e2b47c4bb25681d2c4
joaopedro-1 <jpdr95@gmail.com>
2025-03-17 13:01:09 -0300

    aula01

---
+++ aula01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  Primeiro projeto em C++
+//  Autor: João Pedro 
+//  Data: 17/03/2025
+//
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Iniciando em C++, pelo curso do WrKits!\n";
+    return 0;
+}
